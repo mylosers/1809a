8814e708bcfefe0748a13987bb06a459a426b9a3
losers <losers@localhost.localdomain>
2019-04-23 15:53:34 +0800

    电商页面

---
+++ resources/views/goods/goodslist.blade.php
@@ -0,0 +1,23 @@
+{{--商品列表 --}}
+@extends('layout.bst')
+
+@section('content')
+    <div class="container">
+        <table class="table table-striped">
+            @foreach($data as $k=>$v)
+                <tr>
+                    <td>{{$v['goods_name']}}  </td>
+                    <td>¥ {{$v['goods_price']}}</td>
+                    <td><a href="/goods/{{$v['goods_id']}}">查看商品</a></td>
+                </tr>
+            @endforeach
+        </table>
+        <hr>
+
+    </div>
+
+@endsection
+
+@section('footer')
+    @parent
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,21 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+//首页
+Route::any('/index','Index\indexController@index');
+//购物车
+//Route::get('/cart','Cart\IndexController@index')->middleware('check.uid');
+Route::any('/goods','Cart\IndexController@index');
+Route::any('/goodsList','Cart\IndexController@goodsList'); //商品列表
+Route::any('/goodsSelect','Cart\IndexController@goodsSelect'); //商品搜索
+Route::any('/goodsAdd','Cart\IndexController@goodsAdd');      //添加商品
+Route::any('/number','Cart\IndexController@number');      //判断库存
+Route::any('/goods/{goods_id}','Cart\IndexController@goods');      //添加商品
+Route::any('/goodsDel/{goods_id}','Cart\IndexController@del');      //删除商品
+Route::any('/numGoods','Cart\IndexController@numGoods');      //查询浏览记录
+//订单
+Route::any('/order','Cart\OrderController@orderList');           //订单列表
+Route::any('/orderListAdd','Cart\OrderController@orderListAdd');           //订单列表展示
+Route::any('/orderAdd','Cart\OrderController@add');           //下单
